52a0b58cfeb45aae890ff25d3b0fec2d554d70ff
Evgeny Afanasiev <evgeny.afanasiev@acumatica.com>
2023-05-25 16:44:55 +0300

    Reorganizing root folder, adding Acumatica REST Client NuGet

---
+++ AcumaticaDummyProcessingCenterGatewayAPI/AcumaticaDummyProcessingCenterGatewayAPI/Requests.cs
@@ -0,0 +1,24 @@
+﻿using Acumatica.Auth.Api;
+
+namespace AcumaticaDummyProcessingCenterGatewayAPI
+{
+    public static class Requests
+    {
+        public static string TestConnection(string SiteURL, string Username, string Password, string Tenant = null)
+        {
+            var authApi = new AuthApi(SiteURL);
+            string result;
+            try
+            {
+                var configuration = authApi.LogIn(Username, Password, Tenant, null, null);
+                authApi.TryLogout();
+                result = "Connection to the instance is successful";
+            }
+            catch (Exception e)
+            {
+                result = e.Message;
+            }
+            return result;
+        }
+    }
+}
